b29513c5b7043db1b2b0969f6cf6f755fadc1041
Henrique Chaves <canaldomonza@gmail.com>
2023-01-22 14:45:32 -0300

    feat: main func and test env in jekyll

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const requestMovie = fetch(
+  `https://api.themoviedb.org/3/movie/76341?api_key={%ENV["API_KEY_MOVIEDB"]%}"`
+);
+const movieBox = document.getElementById("movie-container");
+const moviePoster = document.getElementById("poster-movie");
+const movieTitle = document.getElementById("title-movie");
+const movieDescription = document.getElementById("description-movie");
+function main() {
+  requestMovie.then((res) =>
+    res.json().then((data) => {
+      let { original_title, overview, poster_path } = data;
+
+      console.log(data);
+      console.log(original_title);
+      console.log(overview);
+      console.log(poster_path);
+      movieBox.style.display = "flex";
+      moviePoster.setAttribute(
+        "src",
+        `https://image.tmdb.org/t/p/original${poster_path}`
+      );
+      movieTitle.textContent = original_title;
+      movieDescription.textContent = overview;
+    })
+  );
+}
+main();
