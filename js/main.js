4ce2bef922a9675301b473abfb2647c003fd5107
Henrique Chaves <canaldomonza@gmail.com>
2023-01-23 18:50:33 -0300

    feat: add logic controller ui and request api

--- js/main.js
+++ js/main.js
@@ -1,27 +1,30 @@
-const requestMovie = fetch(
-  `https://api.themoviedb.org/3/movie/76341?api_key=284255dfe1f4a024f1acf566b5342314"`
-);
 const movieBox = document.getElementById("movie-container");
 const moviePoster = document.getElementById("poster-movie");
 const movieTitle = document.getElementById("title-movie");
 const movieDescription = document.getElementById("description-movie");
-function main() {
-  requestMovie.then((res) =>
-    res.json().then((data) => {
-      let { original_title, overview, poster_path } = data;
+const btnController = document.getElementById("btn-controller");
+let movieID = 0;
 
-      console.log(data);
-      console.log(original_title);
-      console.log(overview);
-      console.log(poster_path);
-      movieBox.style.display = "flex";
-      moviePoster.setAttribute(
-        "src",
-        `https://image.tmdb.org/t/p/original${poster_path}`
-      );
-      movieTitle.textContent = original_title;
-      movieDescription.textContent = overview;
-    })
-  );
+function main() {
+  btnController.addEventListener("click", () => {
+    function generateNumber() {
+      movieID += Math.floor(Math.random() * 999);
+    }
+    generateNumber();
+    fetch(
+      `https://api.themoviedb.org/3/movie/${movieID}?api_key=284255dfe1f4a024f1acf566b5342314`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        let { original_title, overview, poster_path } = data;
+        movieBox.style.display = "flex";
+        moviePoster.setAttribute(
+          "src",
+          `https://image.tmdb.org/t/p/original${poster_path}`
+        );
+        movieTitle.textContent = original_title;
+        movieDescription.textContent = overview;
+      });
+  });
 }
 main();
