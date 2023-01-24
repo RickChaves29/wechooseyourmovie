12971692c5a7a3b0267d62dc0caa8f76e31016a5
Henrique Chaves <canaldomonza@gmail.com>
2023-01-24 12:58:17 -0300

    feat: add fetch and generate random number

--- js/main.js
+++ js/main.js
@@ -13,18 +13,25 @@ function main() {
     generateNumber();
     fetch(
       `https://api.themoviedb.org/3/movie/${movieID}?api_key=284255dfe1f4a024f1acf566b5342314`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        let { original_title, overview, poster_path } = data;
+    ).then((res) => {
+      if (res.ok == true && res.status == 200) {
+        res.json().then((data) => {
+          let { original_title, overview, poster_path } = data;
+          movieBox.style.display = "flex";
+          moviePoster.setAttribute(
+            "src",
+            `https://image.tmdb.org/t/p/original${poster_path}`
+          );
+          movieTitle.textContent = original_title;
+          movieDescription.textContent = overview;
+        });
+      } else if (res.ok == false && res.status == 404) {
         movieBox.style.display = "flex";
-        moviePoster.setAttribute(
-          "src",
-          `https://image.tmdb.org/t/p/original${poster_path}`
-        );
-        movieTitle.textContent = original_title;
-        movieDescription.textContent = overview;
-      });
+        moviePoster.setAttribute("src", "./assets/poster.png");
+        movieTitle.textContent = `Ops, today is not the day to watch the movie, let's coding! 🚀`;
+        movieDescription.textContent = "";
+      }
+    });
   });
 }
 main();
